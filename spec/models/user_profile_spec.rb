333a961fce6876bf7c16bd700f8c7adb3daf3748
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-05-14 18:35:12 -0500

    Add tests for user_profile status enum. all tests pass.

--- spec/models/user_profile_spec.rb
+++ spec/models/user_profile_spec.rb
@@ -15,4 +15,42 @@ RSpec.describe UserProfile, type: :model do
     it { should validate_presence_of :current_cohort }
     it { should define_enum_for :status }
   end
+
+  describe "user_profile status" do
+    it "user_profile can be enrolled" do
+      user_profile = create(:user_profile, status: 0)
+      expect(user_profile).to be_valid
+      expect(user_profile.status).to eq("enrolled")
+      expect(user_profile.status).to_not eq("graduated")
+      expect(user_profile.status).to_not eq("withdrawn")
+      expect(user_profile.status).to_not eq("on_leave")
+    end
+
+    it "user_profile can be graduated" do
+      user_profile = create(:user_profile, status: 1)
+      expect(user_profile).to be_valid
+      expect(user_profile.status).to eq("graduated")
+      expect(user_profile.status).to_not eq("enrolled")
+      expect(user_profile.status).to_not eq("withdrawn")
+      expect(user_profile.status).to_not eq("on_leave")
+    end
+
+    it "user_profile can be withdrawn" do
+      user_profile = create(:user_profile, status: 2)
+      expect(user_profile).to be_valid
+      expect(user_profile.status).to eq("withdrawn")
+      expect(user_profile.status).to_not eq("graduated")
+      expect(user_profile.status).to_not eq("enrolled")
+      expect(user_profile.status).to_not eq("on_leave")
+    end
+
+    it "user_profile can be on_leave" do
+      user_profile = create(:user_profile, status: 3)
+      expect(user_profile).to be_valid
+      expect(user_profile.status).to eq("on_leave")
+      expect(user_profile.status).to_not eq("enrolled")
+      expect(user_profile.status).to_not eq("graduated")
+      expect(user_profile.status).to_not eq("withdrawn")
+    end
+  end
 end
