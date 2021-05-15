eb05d69455658c308e86e4c02cf75ca0605331bb
Gvieve <gnuebel13@gmail.com>
2021-05-15 16:23:07 -0600

    Remove deleted columns from student_projects factory

--- spec/factories/student_projects.rb
+++ spec/factories/student_projects.rb
@@ -1,12 +1,5 @@
 FactoryBot.define do
   factory :student_project do
-    sequence :name do |n|
-      "Awesome Project #{n + 1}"
-    end
-    sequence :mod  do |n|
-      "#{n + 1}"
-    end
-    program { "BE" }
     instructor_comments { "This is a really good comment" }
     student_comments { "My student comment is better" }
     user
