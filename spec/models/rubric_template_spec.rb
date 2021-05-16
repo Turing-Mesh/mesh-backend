b13045570ce6111323a5c2a5a2b45bcd2c7461dc
Gvieve <gnuebel13@gmail.com>
2021-05-16 09:59:51 -0600

    Add tests and association from rubric_template to rubric_category
    Co-authored-by: Jesus Quezada-Guillen <17693874+jequeza@users.noreply.github.com>

--- spec/models/rubric_template_spec.rb
+++ spec/models/rubric_template_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe RubricTemplate, type: :model do
   describe 'relationships' do
     it {should have_many :rubric_template_categories}
+    it {should have_many(:rubric_categories).through(:rubric_template_categories)}
   end
 end
