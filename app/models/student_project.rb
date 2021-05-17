1487ab89eedd81692cb8815e0a69cab49d89f56f
Gvieve <gnuebel13@gmail.com>
2021-05-17 00:08:17 -0600

    Create test and class method to find student projects by student_id and mod

--- app/models/student_project.rb
+++ app/models/student_project.rb
@@ -4,4 +4,18 @@ class StudentProject < ApplicationRecord
   has_many :project_feedbacks, foreign_key: :project_id
 
   validates_presence_of :student_id, :project_template_id
+
+  def self.student_projects_by_mod(id, mod)
+    select('student_projects.*,
+      project_templates.name,
+      project_templates.mod,
+      project_templates.program,
+      project_templates.project_number,
+      project_templates.is_final,
+      project_templates.rubric_template_id')
+    .joins(:project_template)
+    .where('student_projects.student_id = ?', id)
+    .where('project_templates.mod = ?', mod)
+    .order('project_templates.project_number')
+  end
 end
--- spec/factories/project_feedbacks.rb
+++ spec/factories/project_feedbacks.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     instructor_id {nil}
     project_id {nil}
     rubric_template_category_id {nil}
-    :score { [1, 1.5, 2, 2.5, 3, 3.5, 4].sample }
+    score { [1, 1.5, 2, 2.5, 3, 3.5, 4].sample }
   end
 end
