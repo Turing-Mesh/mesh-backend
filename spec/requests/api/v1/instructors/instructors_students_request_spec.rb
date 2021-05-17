54ce64c5e48fcb673aa44a545991b59b643b7b02
Ben <benjamin.fulton99@gmail.com>
2021-05-17 16:58:55 -0600

    Creates edge case testing for instructor students

--- spec/requests/api/v1/instructors/instructors_students_request_spec.rb
+++ spec/requests/api/v1/instructors/instructors_students_request_spec.rb
@@ -1,23 +1,44 @@
 require 'rails_helper'
 
 describe 'Instructor Students request' do
-  describe 'gets an instructors students' do
-    it "returns a single student's projects and feedback for matching mod when given valid data" do
-      instructor = create(:user, role: 1)
-      student_1 = create(:user, role: 0)
-      student_2 = create(:user, role: 0)
+  before :each do
+    @instructor = create(:user, role: 1)
+    student_1 = create(:user, role: 0)
+    student_2 = create(:user, role: 0)
+    @student_3 = create(:user, role: 0)
 
-      instructor_student_1 = create(:instructor_student, instructor_id: instructor.id, student_id: student_1.id)
-      instructor_student_2 = create(:instructor_student, instructor_id: instructor.id, student_id: student_2.id)
-      student_1_profile = create(:user_profile, user_id: student_1.id, current_mod: "2", starting_cohort: "2011", current_cohort: "2011", status: 0)
-      student_2_profile = create(:user_profile, user_id: student_2.id, current_mod: "2", starting_cohort: "2011", current_cohort: "2011", status: 0)
-      instructor_profile = create(:user_profile, user_id: instructor.id, current_mod: "2")
+    instructor_student_1 = create(:instructor_student, instructor_id: @instructor.id, student_id: student_1.id)
+    instructor_student_2 = create(:instructor_student, instructor_id: @instructor.id, student_id: student_2.id)
+    instructor_student_3 = create(:instructor_student, instructor_id: @instructor.id, student_id: @student_3.id)
+    student_1_profile = create(:user_profile, user_id: student_1.id, current_mod: "2", starting_cohort: "2011", current_cohort: "2011", status: 0)
+    student_2_profile = create(:user_profile, user_id: student_2.id, current_mod: "2", starting_cohort: "2011", current_cohort: "2011", status: 0)
+    student_3_profile = create(:user_profile, user_id: @student_3.id, current_mod: "3", starting_cohort: "2010", current_cohort: "2010", status: 0)
+    instructor_profile = create(:user_profile, user_id: @instructor.id, current_mod: "2")
+  end
+  describe 'happy path' do
+    it "returns information about the students under an instructor" do
+      get "/api/v1/instructors/#{@instructor.id}/instructor_students?mod=2"
+      json = JSON.parse(response.body, symbolize_names: true)
+      
+      expect(json[:data].class).to eq Array
+      expect(json[:data].first[:type]).to eq "student"
+      expect(json[:data].first[:attributes].keys).to eq %i(user_id first_name last_name current_cohort)
+    end
 
-      get "/api/v1/instructors/#{instructor.id}/instructor_students?mod=2"
-      response = JSON.parse(response.body, symbolize_names: true)
+    it "returns only the students in the specified mod" do
+      get "/api/v1/instructors/#{@instructor.id}/instructor_students?mod=2"
+      json = JSON.parse(response.body, symbolize_names: true)
+      expect(json[:data].none?{ |student| student[:attributes][:first_name] == @student_3.user_profile.first_name }).to eq true
+    end
 
-      expect(response[:data].class).to eq Array
-      expect(response[:data].first[:type]).to eq "student"
+
+  end
+
+  describe 'sad path' do
+    it "should return bad data for missing query parameters" do
+      get "/api/v1/instructors/#{@instructor.id}/instructor_students"
+      json = JSON.parse(response.body, symbolize_names: true)
+      expect(json[:data]).to eq "Invalid Query Parameters"
     end
   end
 end
