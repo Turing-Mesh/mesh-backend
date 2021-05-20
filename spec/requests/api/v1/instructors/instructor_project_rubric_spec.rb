e92a202f830ae8292e831625195a5741c6b94844
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-05-19 22:00:00 -0500

    Add test for happy path response.

--- spec/requests/api/v1/instructors/instructor_project_rubric_spec.rb
+++ spec/requests/api/v1/instructors/instructor_project_rubric_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe 'Instructor Project Rubric Request' do
       instructor_profile = create(:user_profile, user_id: @instructor.id, current_mod: "2")
       # 2 rubric_templates, 1 for mod 1, and 1 for mod 2 projects
       mod1_rubric = create(:rubric_template)
-      mod2_rubric = create(:rubric_template)
+      @mod2_rubric = create(:rubric_template)
       # 6 rubric categories
       testing = create(:rubric_category)
       oop = create(:rubric_category)
@@ -20,35 +20,40 @@ RSpec.describe 'Instructor Project Rubric Request' do
       js = create(:rubric_category)
       # 9 rubric_template_categories (4 for mod 1, 5 for mod 2)
       mod1_rubric.rubric_categories << [testing, oop, code_quality, completion]
-      mod2_rubric.rubric_categories << [testing, oop, code_quality, completion, active_record]
+      @mod2_rubric.rubric_categories << [testing, oop, code_quality, completion, active_record]
       # 3 project templates, 1 for each project above
       project_template_1 = create(:project_template, mod: "1", project_number: "4", rubric_template_id: mod1_rubric.id)
-      project_template_2 = create(:project_template, mod: "2", project_number: "3", rubric_template_id: mod2_rubric.id)
-      project_template_3 = create(:project_template, mod: "2", project_number: "4", is_final: true, rubric_template_id: mod2_rubric.id)
-      project_template_4 = create(:project_template, mod: "3", project_number: "1", rubric_template_id: mod2_rubric.id)
+      project_template_2 = create(:project_template, mod: "2", project_number: "2", rubric_template_id: @mod2_rubric.id)
+      project_template_3 = create(:project_template, mod: "2", project_number: "4", is_final: true, rubric_template_id: @mod2_rubric.id)
+      project_template_4 = create(:project_template, mod: "3", project_number: "1", rubric_template_id: @mod2_rubric.id)
       # 3 projects, 1 for mod 1 and 2 for mod 2
       @project_1 = create(:student_project, student_id: @student.id, project_template_id: project_template_1.id)
       @project_2 = create(:student_project, student_id: @student.id, project_template_id: project_template_2.id)
       @project_3 = create(:student_project, student_id: @student.id, project_template_id: project_template_3.id)
-      # 14 project feedback (4 for mod 1 project, 5 for mod 2)
-      # feedback_1 = create(:project_feedback, instructor_id: @instructor.id, project_id: @project_1.id, rubric_template_category_id: mod1_rubric.rubric_template_categories[0].id)
-      # feedback_2 = create(:project_feedback, instructor_id: @instructor.id, project_id: @project_1.id, rubric_template_category_id: mod1_rubric.rubric_template_categories[1].id)
-      # feedback_3 = create(:project_feedback, instructor_id: @instructor.id, project_id: @project_1.id, rubric_template_category_id: mod1_rubric.rubric_template_categories[2].id)
-      # feedback_4 = create(:project_feedback, instructor_id: @instructor.id, project_id: @project_1.id, rubric_template_category_id: mod1_rubric.rubric_template_categories[3].id)
-      # feedback_5 = create(:project_feedback, instructor_id: @instructor.id, project_id: @project_2.id, rubric_template_category_id: mod2_rubric.rubric_template_categories[0].id)
-      # feedback_6 = create(:project_feedback, instructor_id: @instructor.id, project_id: @project_2.id, rubric_template_category_id: mod2_rubric.rubric_template_categories[1].id)
-      # feedback_7 = create(:project_feedback, instructor_id: @instructor.id, project_id: @project_2.id, rubric_template_category_id: mod2_rubric.rubric_template_categories[2].id)
-      # feedback_8 = create(:project_feedback, instructor_id: @instructor.id, project_id: @project_2.id, rubric_template_category_id: mod2_rubric.rubric_template_categories[3].id)
-      # feedback_9 = create(:project_feedback, instructor_id: @instructor.id, project_id: @project_2.id, rubric_template_category_id: mod2_rubric.rubric_template_categories[4].id)
-      # feedback_10 = create(:project_feedback, instructor_id: @instructor.id, project_id: @project_3.id, rubric_template_category_id: mod2_rubric.rubric_template_categories[0].id)
-      # feedback_11 = create(:project_feedback, instructor_id: @instructor.id, project_id: @project_3.id, rubric_template_category_id: mod2_rubric.rubric_template_categories[1].id)
-      # feedback_12 = create(:project_feedback, instructor_id: @instructor.id, project_id: @project_3.id, rubric_template_category_id: mod2_rubric.rubric_template_categories[2].id)
-      # feedback_13 = create(:project_feedback, instructor_id: @instructor.id, project_id: @project_3.id, rubric_template_category_id: mod2_rubric.rubric_template_categories[3].id)
-      # feedback_14 = create(:project_feedback, instructor_id: @instructor.id, project_id: @project_3.id, rubric_template_category_id: mod2_rubric.rubric_template_categories[4].id)
     end
     it "returns rubric categories for the given student, mod, and project number" do
-      get "/api/v1/instructors/#{@instructor.id}/instructor_students/#{@student.id}/project_templates?mod=1&project_number=2"
+      get "/api/v1/instructors/#{@instructor.id}/instructor_students/#{@student.id}/project_templates?mod=2&project_number=2"
       expect(response).to be_successful
+      project_rubric = JSON.parse(response.body, symbolize_names: true)
+      expect(project_rubric).to have_key(:data)
+      expect(project_rubric[:data]).to have_key(:id)
+      expect(project_rubric[:data]).to have_key(:type)
+      expect(project_rubric[:data][:attributes]).to have_key(:student_id)
+      expect(project_rubric[:data][:attributes]).to have_key(:project_template_id)
+      expect(project_rubric[:data][:attributes]).to have_key(:mod)
+      expect(project_rubric[:data][:attributes]).to have_key(:project_number)
+      expect(project_rubric[:data][:attributes]).to have_key(:rubric_template)
+      expect(project_rubric[:data][:attributes][:mod]).to be_a String
+      expect(project_rubric[:data][:attributes][:project_number]).to be_a String
+      expect(project_rubric[:data][:attributes][:rubric_template]).to be_an Array
+      expect(project_rubric[:data][:type]).to eq("project_rubric")
+      expect(project_rubric[:data][:id].to_i).to eq(@project_2.id)
+      expect(project_rubric[:data][:attributes][:rubric_template][0][:rubric_category_name]).to eq(@mod2_rubric.rubric_categories[0].name)
+      expect(project_rubric[:data][:attributes][:rubric_template][1][:rubric_category_name]).to eq(@mod2_rubric.rubric_categories[1].name)
+      expect(project_rubric[:data][:attributes][:rubric_template][2][:rubric_category_name]).to eq(@mod2_rubric.rubric_categories[2].name)
+      expect(project_rubric[:data][:attributes][:rubric_template][3][:rubric_category_name]).to eq(@mod2_rubric.rubric_categories[3].name)
+      expect(project_rubric[:data][:attributes][:rubric_template][4][:rubric_category_name]).to eq(@mod2_rubric.rubric_categories[4].name)
+      expect(project_rubric[:data][:attributes][:student_id]).to eq(@student.id)
     end
   end
 end
