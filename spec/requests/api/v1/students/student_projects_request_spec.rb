b32a3b3bc50000a8a10e60b3a3931ae188ba6f59
Gvieve <gnuebel13@gmail.com>
2021-05-19 18:34:00 -0600

    Add railshelper file for project feedback data, add to request and poros

--- spec/requests/api/v1/students/student_projects_request_spec.rb
+++ spec/requests/api/v1/students/student_projects_request_spec.rb
@@ -1,125 +1,78 @@
 require 'rails_helper'
 
-describe 'Student projects request' do
-  describe 'happy path' do
+RSpec.describe 'Student projects request', type: :request do
+  describe 'returns a students projects' do
     before :each do
-      @student = create(:user, role: 0)
-      @instructor = create(:user, role: 1)
-      instructor_student = create(:instructor_student, instructor_id: @instructor.id, student_id: @student.id)
-      student_profile = create(:user_profile, user_id: @student.id, current_mod: "2", starting_cohort: "2011", current_cohort: "2011", status: 0)
-      instructor_profile = create(:user_profile, user_id: @instructor.id, current_mod: "2")
-      # 2 rubric_templates, 1 for mod 1, and 1 for mod 2 projects
-      mod1_rubric = create(:rubric_template)
-      mod2_rubric = create(:rubric_template)
-      # 6 rubric categories
-      testing = create(:rubric_category)
-      oop = create(:rubric_category)
-      code_quality = create(:rubric_category)
-      completion = create(:rubric_category)
-      active_record = create(:rubric_category)
-      js = create(:rubric_category)
-      # 9 rubric_template_categories (4 for mod 1, 5 for mod 2)
-      mod1_rubric.rubric_categories << [testing, oop, code_quality, completion]
-      mod2_rubric.rubric_categories << [testing, oop, code_quality, completion, active_record]
-      # 3 project templates, 1 for each project above
-      project_template_1 = create(:project_template, mod: "1", project_number: "4", rubric_template_id: mod1_rubric.id)
-      project_template_2 = create(:project_template, mod: "2", project_number: "3", rubric_template_id: mod2_rubric.id)
-      project_template_3 = create(:project_template, mod: "2", project_number: "4", is_final: true, rubric_template_id: mod2_rubric.id)
-      project_template_4 = create(:project_template, mod: "3", project_number: "1", rubric_template_id: mod2_rubric.id)
-      # 3 projects, 1 for mod 1 and 2 for mod 2
-      @project_1 = create(:student_project, student_id: @student.id, project_template_id: project_template_1.id)
-      @project_2 = create(:student_project, student_id: @student.id, project_template_id: project_template_2.id)
-      @project_3 = create(:student_project, student_id: @student.id, project_template_id: project_template_3.id)
-      # 14 project feedback (4 for mod 1 project, 5 for mod 2)
-      feedback_1 = create(:project_feedback, instructor_id: @instructor.id, project_id: @project_1.id, rubric_template_category_id: mod1_rubric.rubric_template_categories[0].id)
-      feedback_2 = create(:project_feedback, instructor_id: @instructor.id, project_id: @project_1.id, rubric_template_category_id: mod1_rubric.rubric_template_categories[1].id)
-      feedback_3 = create(:project_feedback, instructor_id: @instructor.id, project_id: @project_1.id, rubric_template_category_id: mod1_rubric.rubric_template_categories[2].id)
-      feedback_4 = create(:project_feedback, instructor_id: @instructor.id, project_id: @project_1.id, rubric_template_category_id: mod1_rubric.rubric_template_categories[3].id)
-      feedback_5 = create(:project_feedback, instructor_id: @instructor.id, project_id: @project_2.id, rubric_template_category_id: mod2_rubric.rubric_template_categories[0].id)
-      feedback_6 = create(:project_feedback, instructor_id: @instructor.id, project_id: @project_2.id, rubric_template_category_id: mod2_rubric.rubric_template_categories[1].id)
-      feedback_7 = create(:project_feedback, instructor_id: @instructor.id, project_id: @project_2.id, rubric_template_category_id: mod2_rubric.rubric_template_categories[2].id)
-      feedback_8 = create(:project_feedback, instructor_id: @instructor.id, project_id: @project_2.id, rubric_template_category_id: mod2_rubric.rubric_template_categories[3].id)
-      feedback_9 = create(:project_feedback, instructor_id: @instructor.id, project_id: @project_2.id, rubric_template_category_id: mod2_rubric.rubric_template_categories[4].id)
-      feedback_10 = create(:project_feedback, instructor_id: @instructor.id, project_id: @project_3.id, rubric_template_category_id: mod2_rubric.rubric_template_categories[0].id)
-      feedback_11 = create(:project_feedback, instructor_id: @instructor.id, project_id: @project_3.id, rubric_template_category_id: mod2_rubric.rubric_template_categories[1].id)
-      feedback_12 = create(:project_feedback, instructor_id: @instructor.id, project_id: @project_3.id, rubric_template_category_id: mod2_rubric.rubric_template_categories[2].id)
-      feedback_13 = create(:project_feedback, instructor_id: @instructor.id, project_id: @project_3.id, rubric_template_category_id: mod2_rubric.rubric_template_categories[3].id)
-      feedback_14 = create(:project_feedback, instructor_id: @instructor.id, project_id: @project_3.id, rubric_template_category_id: mod2_rubric.rubric_template_categories[4].id)
+      seed_test_db
     end
+    describe 'happy path' do
+      it "returns a single student's projects and feedback for matching mod when given valid data" do
+        mod = 2
 
-    it "returns a single student's projects and feedback for matching mod when given valid data" do
-      mod = 2
+        get "/api/v1/students/#{@student.id}/student_projects?mod=#{mod}"
 
-      get "/api/v1/students/#{@student.id}/student_projects?mod=#{mod}"
+        json = JSON.parse(response.body, symbolize_names:true)
 
-      json = JSON.parse(response.body, symbolize_names:true)
+        expect(response).to be_successful
+        expect(json).to be_a(Hash)
+        expect(json[:data]).to be_a(Hash)
+        check_hash_structure(json[:data], :id, String)
+        check_hash_structure(json[:data], :type, String)
+        expect(json[:data][:type]).to eq("projects")
+        expect(json[:data][:attributes]).to be_a(Hash)
+        check_hash_structure(json[:data][:attributes], :mod, String)
+        check_hash_structure(json[:data][:attributes], :student_projects, Array)
+        expect(json[:data][:attributes][:student_projects].count).to eq(2)
+        check_hash_structure(json[:data][:attributes][:student_projects].first, :project_feedback, Array)
+        expect(json[:data][:attributes][:student_projects].first[:project_feedback].count).to eq(5)
+      end
 
-      expect(response).to be_successful
-      expect(json).to be_a(Hash)
-      expect(json[:data]).to be_a(Hash)
-      expect(json[:data]).to have_key(:id)
-      expect(json[:data][:id]).to be_a(String)
-      expect(json[:data][:id]).to eq(@student.id.to_s)
-      expect(json[:data]).to have_key(:type)
-      expect(json[:data][:type]).to eq("projects")
-      expect(json[:data][:attributes]).to be_a(Hash)
-      expect(json[:data][:attributes]).to have_key(:mod)
-      expect(json[:data][:attributes][:mod]).to be_a(String)
-      expect(json[:data][:attributes][:mod]).to eq(mod.to_s)
-      expect(json[:data][:attributes]).to have_key(:student_projects)
-      expect(json[:data][:attributes][:student_projects]).to be_an(Array)
-      expect(json[:data][:attributes][:student_projects].count).to eq(2)
-      expect(json[:data][:attributes]).to have_key(:student_projects)
-      expect(json[:data][:attributes][:student_projects].first).to have_key(:project_feedback)
-      expect(json[:data][:attributes][:student_projects].first[:project_feedback]).to be_an(Array)
-      expect(json[:data][:attributes][:student_projects].first[:project_feedback].count).to eq(5)
-    end
+      it "the projects response for valid request includes student project data" do
+        mod = 2
 
-    it "the projects response for valid request includes student project data" do
-      mod = 2
+        get "/api/v1/students/#{@student.id}/student_projects?mod=#{mod}"
 
-      get "/api/v1/students/#{@student.id}/student_projects?mod=#{mod}"
+        json = JSON.parse(response.body, symbolize_names:true)
+        project = json[:data][:attributes][:student_projects].first
 
-      json = JSON.parse(response.body, symbolize_names:true)
-      project = json[:data][:attributes][:student_projects].first
+        check_hash_structure(project, :id, Integer)
+        check_hash_structure(project, :name, String)
+        check_hash_structure(project, :program, String)
+        check_hash_structure(project, :project_number, String)
+        check_hash_structure(project, :project_type, String)
+        expect(project[:is_final_project]).to be_a(FalseClass).or be_a(TrueClass)
+        check_hash_structure(project, :average_score, String)
+        check_hash_structure(project, :instructor_comments, String)
+        check_hash_structure(project, :student_comments, String)
+      end
 
-      expect(project[:id]).to be_a(Integer)
-      expect(project[:name]).to be_a(String)
-      expect(project[:program]).to be_a(String)
-      expect(project[:project_number]).to be_a(String)
-      expect(project[:project_type]).to be_a(String)
-      expect(project[:is_final_project]).to be_a(FalseClass).or be_a(TrueClass)
-      expect(project[:average_score]).to be_a(String)
-      expect(project[:instructor_comments]).to be_a(String)
-      expect(project[:student_comments]).to be_a(String)
-    end
+      it "the projects response for a valid request includes project feedback data" do
+        mod = 2
 
-    it "the projects response for a valid request includes project feedback data" do
-      mod = 2
+        get "/api/v1/students/#{@student.id}/student_projects?mod=#{mod}"
 
-      get "/api/v1/students/#{@student.id}/student_projects?mod=#{mod}"
+        json = JSON.parse(response.body, symbolize_names:true)
+        feedback = json[:data][:attributes][:student_projects].first[:project_feedback].first
 
-      json = JSON.parse(response.body, symbolize_names:true)
-      feedback = json[:data][:attributes][:student_projects].first[:project_feedback].first
+        check_hash_structure(feedback, :id, Integer)
+        check_hash_structure(feedback, :rubric_category_name, String)
+        check_hash_structure(feedback, :score, Float)
+        expect(feedback[:comment]).to be_a(String).or be_nil
+      end
 
-      expect(feedback[:id]).to be_a(Integer)
-      expect(feedback[:rubric_category_name]).to be_a(String)
-      expect(feedback[:score]).to be_a(Float)
-      expect(feedback[:comment]).to be_a(String).or be_nil
-    end
+      it "the projects response does not include projects for different mod" do
+        mod = 2
 
-    it "the projects response does not include projects for different mod" do
-      mod = 2
+        get "/api/v1/students/#{@student.id}/student_projects?mod=#{mod}"
 
-      get "/api/v1/students/#{@student.id}/student_projects?mod=#{mod}"
+        json = JSON.parse(response.body, symbolize_names:true)
+        projects = json[:data][:attributes][:student_projects]
 
-      json = JSON.parse(response.body, symbolize_names:true)
-      projects = json[:data][:attributes][:student_projects]
-
-      expect(projects.count).to eq(2)
-      expect(projects.first[:id]).to eq(@project_2.id)
-      expect(projects.last[:id]).to eq(@project_3.id)
-      expect(projects.include?(@project_1.id)).to eq(false)
+        expect(projects.count).to eq(2)
+        expect(projects.first[:id]).to eq(@project_2.id)
+        expect(projects.last[:id]).to eq(@project_3.id)
+        expect(projects.include?(@project_1.id)).to eq(false)
+      end
     end
   end
 
@@ -135,7 +88,6 @@ describe 'Student projects request' do
 
       expect(response).to be_successful
       expect(json[:data][:attributes][:student_projects]).to be_nil
-      expect(json[:data][:attributes][:project_feedback]).to be_nil
     end
   end
 
