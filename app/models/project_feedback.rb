45e966d11efa1e244e41484cbe8382999cc33da3
Gvieve <gnuebel13@gmail.com>
2021-05-26 10:43:42 -0600

    Add method on project_feedback to create records from request

--- app/models/project_feedback.rb
+++ app/models/project_feedback.rb
@@ -5,4 +5,16 @@ class ProjectFeedback < ApplicationRecord
   has_one :rubric_category, through: :rubric_template_category
 
   validates_presence_of :score
+
+  def self.create_feedback(project, params)
+    params[:project_feedback].each do |feedback|
+      create!(
+        instructor_id: params[:instructor_id].to_i,
+        project_id: project.id,
+        rubric_template_category_id: feedback[:rubric_template_category_id],
+        score: feedback[:score],
+        comment: feedback[:comment]
+      )
+    end
+  end
 end
